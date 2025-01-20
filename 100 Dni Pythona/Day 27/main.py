57620f3de0b18489b5ed9c3effbd6d30cb951683
marcin <lukasikmarcin0@gmail.com>
2025-01-20 20:24:18 +0100

    dzień 27

--- 100 Dni Pythona/Day 27/main.py
+++ 100 Dni Pythona/Day 27/main.py
@@ -1,24 +1,29 @@
-import  tkinter
+from tkinter import *
 
-from PIL.ImageOps import expand
-
-window = tkinter.Tk()
-
+window = Tk()
 window.title("My first GUI program")
 window.minsize(500,300)
+window.config(padx=20, pady=20)
+
+# Label
+my_label = Label(text='I Am a label', font=("Arial", 24, "bold"))
+my_label["text"] = 'new text'
+my_label.config(text="New text")
+my_label.grid(column=0, row=0)
 
 
+# def button_click():
+#     my_label["text"] = input.get()
 
-my_label = tkinter.Label(text='I Am a label', font=("Arial", 24, "bold"))
-my_label.pack(expand=True)
+# Button
+button = Button(text="Click me")
+button.grid(column = 1, row =1)
 
-
-
-
-
-
-
-
+button2 = Button(text='New Button')
+button2.grid(column=2, row=0)
+# Input
+input = Entry(width = 10)
+input.grid(column=3,row=2)
 
 
 window.mainloop()
--- 100 Dni Pythona/Day 27/playground.py
+++ 100 Dni Pythona/Day 27/playground.py
@@ -22,8 +22,8 @@ calculate(2,add=3, multiply=5)
 
 class Car:
     def __init__(self, **kw):
-        self.make = kw["make"]
-        self.model = kw["model"]
+        self.make = kw.get("make")
+        self.model = kw.get("model")
 
 my_car = Car(make="Nissan", model="GTR")
 print(my_car.make)
