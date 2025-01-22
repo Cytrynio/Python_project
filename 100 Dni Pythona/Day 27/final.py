d4246b3c7a1cd98261f3d995df6324573e04dc7f
marcin <lukasikmarcin0@gmail.com>
2025-01-22 16:10:59 +0100

    pomodoro project

--- 100 Dni Pythona/Day 27/final.py
+++ 100 Dni Pythona/Day 27/final.py
@@ -2,11 +2,9 @@ from tkinter import *
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(300,200)
 window.config(padx=20, pady=20)
 
-empty_label = Label()
-empty_label.grid(column=0,row=0)
+
 
 input = Entry(width=10)
 input.grid(column=2, row=1)
@@ -26,6 +24,7 @@ km_label.grid(column=3,row=3)
 def miles_to_km():
     result = int(input.get()) * 1.60934
     calculation["text"] = round(result,2)
+
 button = Button(text="Calculate", command=miles_to_km)
 button.grid(column=2,row=4)
 
