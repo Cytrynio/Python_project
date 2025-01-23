e6ad08b039098455f7b8361e34d1c9842117db92
marcin <lukasikmarcin0@gmail.com>
2025-01-23 12:03:14 +0100

    PASSWORD GENERATOR vol2

--- 100 Dni Pythona/Day 29 /password-manager-start/main.py
+++ 100 Dni Pythona/Day 29 /password-manager-start/main.py
@@ -1,10 +1,40 @@
 from tkinter import *
-
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    all_characters = letters + numbers + symbols
+
+    password_list = [random.choice(all_characters) for _ in range(nr_numbers + nr_letters + nr_symbols)]
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0,password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
-
+def save():
+    website = website_input.get()
+    email = email_username_input.get()
+    password = password_input.get()
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered \nEmail: {email} \nPassword: {password}\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                website_input.delete(0,END)
+                password_input.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -33,7 +63,7 @@ website_input.focus()
 
 email_username_input = Entry(width=35)
 email_username_input.grid(column=1,row=2, columnspan=2)
-email_username_input.insert(END)
+email_username_input.insert(0, 'xxx@wp.pl')
 
 password_input = Entry(width=24)
 password_input.grid(column=1,row=3)
@@ -41,10 +71,10 @@ password_input.grid(column=1,row=3)
 
 # Buttons
 
-generate_password_btn = Button(text="Generate",width=8)
+generate_password_btn = Button(text="Generate",width=8,command=password_generator)
 generate_password_btn.grid(column=2,row=3)
 
-add_btn= Button(text="Add",width=32)
+add_btn= Button(text="Add",width=32,command=save)
 add_btn.grid(columnspan=2, column=1,row=4)
 
 
