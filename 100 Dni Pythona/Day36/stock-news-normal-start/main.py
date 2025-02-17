5f3117393087cfce40c89c6f5ed0cdb760ff9271
marcin <lukasikmarcin0@gmail.com>
2025-02-17 17:48:15 +0100

    trading app

---
+++ 100 Dni Pythona/Day36/stock-news-normal-start/main.py
@@ -0,0 +1,112 @@
+import random
+import requests
+import os
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API = os.environ.get('STOCK_API')
+NEWS_API = os.environ.get('NEW_API')
+
+account_sid = "ACe1bddcb8b1b001e5693d1575bafef782"
+auth_token =  os.environ.get("AUTH_TOKEN")
+
+
+# --- STOCK DATA ---
+
+def get_stock_data():
+
+    stock_parameters = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol":STOCK_NAME,
+        "apikey":STOCK_API
+    }
+
+    stock_response = requests.get(STOCK_ENDPOINT, params=stock_parameters)
+    try:
+        stock_response.raise_for_status()
+        data = stock_response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
+def get_dates(data):
+    try:
+        dates = sorted(data['Time Series (Daily)'].keys(), reverse=True)
+        return dates
+    except KeyError:
+        print("You used 25 request per day")
+        return None
+
+def calculate_change(yesterday_close, day_before_yesterday_close):
+    return round(((float(yesterday_close) - float(day_before_yesterday_close)) / float(day_before_yesterday_close)) * 100, 2)
+
+# --- NEWS DATA ---
+
+def get_news(dates):
+    news_parameters = {
+        "q": COMPANY_NAME,
+        "from": dates[0],
+        "sortBy": "popularity",
+        "apiKey": NEWS_API
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_parameters)
+    try:
+        news_response.raise_for_status()
+        news_data = news_response.json()["articles"][:3]
+        news_list = [[article.get('title', 'Brak tytułu'), article.get('description', 'Brak opisu')] for article in news_data]
+        print(news_list)
+        return news_list
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
+
+def main():
+    data = get_stock_data()
+    dates = get_dates(data)
+    if dates:
+        yesterday_date = dates[0]
+        day_before_yesterday_date = dates[1]
+        yesterday_close_price = data['Time Series (Daily)'][yesterday_date]["4. close"]
+        day_before_yesterday_close_price = data["Time Series (Daily)"][day_before_yesterday_date]["4. close"]
+        change = calculate_change(yesterday_close_price,day_before_yesterday_close_price)
+
+        articles = get_news(dates)
+        rand_articles = random.choice(articles)
+        rand_news_title = rand_articles[0]
+        rand_news_description = rand_articles[1]
+        print(rand_articles)
+        if change > 0.01:
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                body=f"{COMPANY_NAME}: 🔺{change}%"
+                     f"Headline: {rand_news_title}({STOCK_NAME})?. "
+                     f"Brief: {rand_news_description}",
+                from_="+18573418987",
+                to="+48692313074",
+            )
+            print("Stock raises")
+            print(message.status)
+        elif change < -0.01:
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                body=f"{COMPANY_NAME}: 🔻{change}%"
+                     f"Headline: {rand_news_title}({STOCK_NAME})?. "
+                     f"Brief: {rand_news_description}",
+                from_="+18573418987",
+                to="+48692313074",
+            )
+            print("Stock dropped")
+            print(message.status)
+        else:
+            print(f"{change:.2f}%")
+
+
+main()
